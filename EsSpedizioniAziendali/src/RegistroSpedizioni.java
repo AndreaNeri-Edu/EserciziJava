4b4deb6b0de482cdba8a7f53b6e0da935bb177c2
edu <andrea.nerimocchi@alessandrinimainardi.edu.it>
2026-01-14 13:45:32 +0100

    blu ma non viola

--- EsSpedizioniAziendali/src/RegistroSpedizioni.java
+++ EsSpedizioniAziendali/src/RegistroSpedizioni.java
@@ -1,3 +1,20 @@
+import java.util.ArrayList;
+
 public class RegistroSpedizioni {
-    
+    private ArrayList<Spedizione> registro;
+
+    public RegistroSpedizioni() { registro = new ArrayList<>(); }
+
+    public void registraSpedizione(Spedizione spedi) { registro.add(spedi); }
+
+    public void visualizzaSpedizioni() {
+        if (registro.isEmpty()) { System.out.println("Nessuna spedizione registrata"); }
+        else {
+            for (Spedizione spedi : registro) {
+                spedi.scriviOutput();
+                System.out.println("----------------------");
+            }
+        }
+    }
+
 }
